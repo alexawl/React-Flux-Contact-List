9c165b9ce361d2bd519c4a52aea83140e80db762
alexawl <alexawl@bellsouth.net>
2019-12-14 17:00:57 +0000

    4thEdit

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
 
-// import ScrollToTop from "./component/scrollToTop";
-
+import { Home } from "./views/home";
+import { Demo } from "./views/demo";
+import { AddContact } from "./views/addContact";
 import { EditContact } from "./views/editContact";
-import injectContext from "./store/appContext";
 import { Contacts } from "./views/contacts";
-import { AddContact } from "./views/addContact.js";
+import { Single } from "./views/single";
+import injectContext from "./store/appContext";
+
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
 
 export const Layout = () => {
 	const basename = process.env.BASENAME || "";
@@ -15,9 +20,9 @@ export const Layout = () => {
 			<BrowserRouter>
 				<Switch>
 					<Route path="/add-contact/:thetype" component={AddContact} />
-					<Route exact path="/index.html" component={Contacts} />
-					<Route path="/contacts/:type" component={Contacts} />
+					<Route path="/contacts/" component={Contacts} />
 					<Route path="/edit-contact/:theindex" component={EditContact} />
+					<Route path="/" component={Contacts} />
 					<Route render={() => <h1>Not found!</h1>} />
 				</Switch>
 			</BrowserRouter>
